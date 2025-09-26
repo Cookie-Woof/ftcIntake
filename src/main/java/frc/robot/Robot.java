1a53f1d1bb5d41e54b9b20475b761fca568df5ac
Ariel <notwoofcookie@gmail.com>
2025-09-26 11:46:24 +0100

    Simple Swarve drive that acts like a tank needs update and swarve math

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,30 +7,109 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 // import pabeles.concurrency.IntRangeObjectConsumer;
-
+import frc.robot.Robot.SwerveSubsystem;
 // import java.security.PublicKey;
 import edu.wpi.first.wpilibj.Timer;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 // import edu.wpi.first.wpilibj.MotorSafety;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Robot extends TimedRobot {
   private XboxController controller;
   private WPI_TalonSRX motor;
   private DigitalInput sensor;
   private Timer intakeTimer;
+  private SwerveSubsystem swerve = new SwerveSubsystem();
+
+ //Swarve drive ------------------------------------
+
+//swarve drive ----------------------------------------
+public class SwerveSubsystem extends SubsystemBase {
+
+  private SwerveModule frontLeft = new SwerveModule(1, 2);   // Drive=1, Steer=2
+  private SwerveModule frontRight = new SwerveModule(3, 4);  // Drive=3, Steer=4
+  private SwerveModule backLeft = new SwerveModule(5, 6);    // Drive=5, Steer=6
+  private SwerveModule backRight = new SwerveModule(7, 8);   // Drive=7, Steer=8
+
+  public SwerveSubsystem() {
+    System.out.println("Swerve subsystem created with 4 modules!");
+  }
+
+  public void testAllWheels() {
+    frontLeft.setDriveSpeed(0.3);
+    frontRight.setDriveSpeed(0.3);
+    backLeft.setDriveSpeed(0.3);
+    backRight.SetDriveSpeed(0.3);
+}
+public void stopAll() {
+  frontLeft.stop();
+  frontRight.stop();
+  backLeft.stop();
+  backRight.stop();
+}
+}
+
+public void simpleDrive(XboxController controller) {
+  double forward = -controller.getLeftY();  // Forward/backward
+  double turn = controller.getRightX();     // Left/right turn
+  
+  // Tank drive math
+  double leftSpeed = forward + turn;
+  double rightSpeed = forward - turn;
+  
+  // Left side modules
+  frontLeft.setDriveSpeed(leftSpeed);
+  backLeft.setDriveSpeed(leftSpeed);
+  
+  // Right side modules  
+  frontRight.setDriveSpeed(rightSpeed);
+  backRight.setDriveSpeed(rightSpeed);
+}
+
+
+public class SwerveModule {
+  private TalonFX driveMotor;
+  private TalonFX turnMotor;
+  
+  public SwerveModule(int driveID, int turnID) {
+      driveMotor = new TalonFX(driveID);
+      turnMotor = new TalonFX(turnID);
+
+      driveMotor.setNeutralMode(NeutralModeValue.Brake);
+      turnMotor.setNeutralMode(NeutralModeValue.Brake);
+  
+  }
+  public void SetDriveSpeed() {
+    driveMotor.set(speed);
+  }
+  public void SetTurnSpeed() {
+    turnMotor.set(speed);
+  }
+  public void stop() {
+    driveMotor.set(0);
+    turnMotor.set(0);
+  }
+  
+  // Methods to control this wheel
+}
 
   public Robot() {
 
   }
   @Override
   public void robotInit() {
-    motor = new WPI_TalonSRX(1); // change id later to the one you need
+    //ftc intake motor workshop
+    motor = new WPI_TalonSRX(1); // change id later to the one you need, BS: chnage the WPI_TalonSRX to the kind of motor that ur using in here were using Falcon 500
     controller = new XboxController(0);
     sensor = new DigitalInput(0); // DIO port 0
     intakeTimer = new Timer();
     System.out.println("Motor robot initialized! along with the sensor");
+    //swarve drivertrain
+
   }
 
 // this function is getting called every 20ms 
@@ -86,11 +165,13 @@ public class Robot extends TimedRobot {
       intakeTimer.stop();
       intakeTimer.reset();
     }
+    swerve.simpleDrive(controller);
   }
   //This function is called once when the robot is disabled. 
   @Override
   public void disabledInit() {
     motor.stopMotor();
+    swerve.stopAll();
   }
 
   // This function is called periodically when disabled. 
@@ -113,4 +194,3 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationPeriodic() {}
 }
-// hellooooo
